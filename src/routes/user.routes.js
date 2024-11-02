dfda9a8671d72a80b2eeac3c293547ad9a75329f
Madhur <sharma15madhur@gmail.com>
2024-11-02 11:53:10 +0530

    used aggregation pipeline to count subscribers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { loginUser,logoutUser,registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser,logoutUser,registerUser,refreshAccessToken, changeCurrentPassword } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -20,7 +20,12 @@ router.route("/login").post(loginUser)
 
 // secured routes
 
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(
+    verifyJWT,
+    logoutUser
+)
+router.route("/refresh-token").post(
+    refreshAccessToken)
+
 
 export default router
